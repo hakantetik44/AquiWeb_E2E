a3d4fbd432a0ded597a29498d53f1aa05baf1b0a
hakantetik44 <fildisili88@gmail.com>
2025-03-26 23:28:21 +0100

    Update Cypress configuration for headless mode and fix dependencies

--- cypress.config.js
+++ cypress.config.js
@@ -5,10 +5,17 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       return config;
     },
+    experimentalStudio: true,
     baseUrl: 'https://www.astree-software.fr/',
     defaultCommandTimeout: 10000,
     viewportWidth: 1920,
     viewportHeight: 1080,
-    chromeWebSecurity: false
+    video: false,
+    screenshotOnRunFailure: true,
+    chromeWebSecurity: false,
+    retries: {
+      runMode: 2,
+      openMode: 0
+    }
   },
 });
