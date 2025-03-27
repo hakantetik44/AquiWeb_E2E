ac2fb83401e2bc6991919549422a9a7522781804
hakantetik44 <fildisili88@gmail.com>
2025-03-27 12:43:53 +0100

    fix: update Cypress configuration and dependencies

--- cypress.config.js
+++ cypress.config.js
@@ -1,25 +1,17 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor')
+const preprocessor = require('@badeball/cypress-cucumber-preprocessor')
+const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on)
-    },
-    experimentalStudio: true,
+    specPattern: 'cypress/e2e/**/*.feature',
+    supportFile: 'cypress/support/e2e.js',
     baseUrl: 'https://www.aquiweb.fr',
-    defaultCommandTimeout: 10000,
     viewportWidth: 1920,
     viewportHeight: 1080,
     video: true,
     screenshotOnRunFailure: true,
-    chromeWebSecurity: false,
-    retries: {
-      runMode: 2,
-      openMode: 0
-    },
-    env: {
-      allure: true
-    },
     reporter: 'cypress-mochawesome-reporter',
     reporterOptions: {
       charts: true,
@@ -31,6 +23,15 @@ module.exports = defineConfig({
       html: false,
       json: true,
       reportDir: 'test-reports'
-    }
+    },
+    setupNodeEvents(on, config) {
+      on('file:preprocessor',
+        createBundler({
+          plugins: [createEsbuildPlugin.default(config)],
+        })
+      )
+      require('cypress-mochawesome-reporter/plugin')(on)
+      return config
+    },
   },
-});
+})
