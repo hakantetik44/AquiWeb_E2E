9fdf82a6826bb60dfe040a0da7bbee6a3442f78d
hakantetik44 <fildisili88@gmail.com>
2025-03-27 12:48:03 +0100

    refactor: remove old test files and update support file

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,5 +1,20 @@
 import '@shelex/cypress-allure-plugin';
 
+// Import commands.js using ES2015 syntax:
+import './commands'
+
+// Import cucumber steps
+import './step_definitions/homepage'
+
+// Hide XHR requests from command log
+const app = window.top;
+if (app) {
+  app.console.log = () => {};
+}
+
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
+
 beforeEach(() => {
     cy.log('Test başlıyor');
     Cypress.on('uncaught:exception', (err, runnable) => {
